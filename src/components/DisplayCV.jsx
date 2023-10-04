5e0034f00db6fbf8ccd061272c7458565cac6020
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-10-04 11:38:21 +0300

    Updated design

--- src/components/DisplayCV.jsx
+++ src/components/DisplayCV.jsx
@@ -3,29 +3,40 @@ import PropTypes from 'prop-types';
 const DisplayCV = ({ personalInfo, educationInfo, practicalExpInfo }) => {
 	return (
 		<div className="cv-display">
-			<h2 className="cv-title">CV</h2>
+			<div className="cv-title">
+				<h2>CV</h2>
+			</div>
 
 			<div className="cv-display-sections">
 				<div className="cv-section">
-					<p className="label-text">Full Name: {personalInfo.fullName}</p>
-					<p className="label-text">Email: {personalInfo.email}</p>
-					<p className="label-text">Phone Number: {personalInfo.phoneNumber}</p>
+					<div className="cv-section-title">Personal Info</div>
+					<div className="section-info">
+						<p className="label-text">Full Name: {personalInfo.fullName}</p>
+						<p className="label-text">Email: {personalInfo.email}</p>
+						<p className="label-text">Phone Number: {personalInfo.phoneNumber}</p>
+					</div>
 				</div>
 
 				<div className="cv-section">
-					<p className="label-text">School Name: {educationInfo.schoolName}</p>
-					<p className="label-text">Title Of Study: {educationInfo.titleOfStudy}</p>
-					<p className="label-text">Date Of Study: {educationInfo.dateOfStudy}</p>
+					<div className="cv-section-title">Education</div>
+					<div className="section-info">
+						<p className="label-text">School Name: {educationInfo.schoolName}</p>
+						<p className="label-text">Title Of Study: {educationInfo.titleOfStudy}</p>
+						<p className="label-text">Date Of Study: {educationInfo.dateOfStudy}</p>
+					</div>
 				</div>
 
 				<div className="cv-section">
-					<p className="label-text">Company Name: {practicalExpInfo.companyName}</p>
-					<p className="label-text">Position Title: {practicalExpInfo.positionTitle}</p>
-					<p className="label-text">
-						Main Responsibilities: {practicalExpInfo.mainResponsibilities}
-					</p>
-					<p className="label-text">Date Started: {practicalExpInfo.dateStarted}</p>
-					<p className="label-text">Date Finished: {practicalExpInfo.dateFinished}</p>
+					<div className="cv-section-title">Practical Experience</div>
+					<div className="section-info">
+						<p className="label-text">Company Name: {practicalExpInfo.companyName}</p>
+						<p className="label-text">Position Title: {practicalExpInfo.positionTitle}</p>
+						<p className="label-text">
+							Main Responsibilities: {practicalExpInfo.mainResponsibilities}
+						</p>
+						<p className="label-text">Date Started: {practicalExpInfo.dateStarted}</p>
+						<p className="label-text">Date Finished: {practicalExpInfo.dateFinished}</p>
+					</div>
 				</div>
 			</div>
 		</div>
